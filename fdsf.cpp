876ba4241c16bcdbc88c7229c7dfa165779bcc6d
semenak94 <semenak94@mail.ru>
2016-04-24 21:32:56 +0300

    Small refactoring

--- fdsf.cpp
+++ fdsf.cpp
@@ -7,20 +7,20 @@
 using namespace fdsf;
 
 // Значение pi
-const cpp_dec_float_50 PI = boost::math::constants::pi<cpp_dec_float_50>();
+const bmp_real PI = boost::math::constants::pi<bmp_real>();
 
 // Значение Ik(0) для индекса k = 0,1,2,3
-//const static cpp_dec_float_50 I_k_0[] = { log(cpp_dec_float_50(2))
+//const static bmp_real I_k_0[] = { log(bmp_real(2))
 #if 0
-cpp_dec_float_50 fdsf::get_T_max(cpp_dec_float_50 X, int k)
+bmp_real fdsf::get_T_max(bmp_real X, int k)
 {
-    cpp_dec_float_50 a1;
+    bmp_real a1;
     if (k != 0) {
         a1 = pow(fdsf::factorial(k + 1), -1 / k);
     }
     int i = 1;
-    cpp_dec_float_50 y = log(1 + exp(X));
-    cpp_dec_float_50 T = X - log(epsilon), I_approximate;
+    bmp_real y = log(1 + exp(X));
+    bmp_real T = X - log(epsilon), I_approximate;
     while (true)
     {
         // 3-х итераций вполне достаточно для определения Tmax
@@ -43,12 +43,12 @@ cpp_dec_float_50 fdsf::get_T_max(cpp_dec_float_50 X, int k)
 #endif
 
 // Новая схема Горнера для прецизионного вычисления y
-static cpp_dec_float_50 GornerSchemeForPrecesionY(int N, cpp_dec_float_50 x)
+static bmp_real GornerSchemeForPrecesionY(int N, bmp_real x)
 {
-    //cpp_dec_float_50 alpha = x < epsilon ? exp(x) : exp(-x);
-    const cpp_dec_float_50 alpha = exp(x);
-    const cpp_dec_float_50 z = alpha / (2 + alpha);
-    cpp_dec_float_50 sum = 1.0 / (2 * N + 1);
+    //bmp_real alpha = x < epsilon ? exp(x) : exp(-x);
+    const bmp_real alpha = exp(x);
+    const bmp_real z = alpha / (2 + alpha);
+    bmp_real sum = 1.0 / (2 * N + 1);
 
     for (int i = N - 1; i >= 0; i--) {
         sum = 1.0 / (2 * i + 1.0) + z*z*sum;
@@ -58,23 +58,25 @@ static cpp_dec_float_50 GornerSchemeForPrecesionY(int N, cpp_dec_float_50 x)
     return 2 * z*sum;
 }
 
-cpp_dec_float_50 fdsf::FermiDirakFunction(cpp_dec_float_50 t, cpp_dec_float_50 x, cpp_dec_float_50 k)
+// Вычисляет значение функции ФД индекса k = 1, 2, 3 в точке x при заданном
+// значении t. Функция представлена в виде t^k /(exp(x)+exp(t))
+static bmp_real FermiDirakFunction(bmp_real t, bmp_real x, bmp_real k)
 {
 #if 0
     // new , зачин для полуцелых
     return 2 * pow(t, 2 * k + 1) / (1 + exp(t*t - x));
 #endif
-    return pow(t, k) / (boost::math::tgamma(cpp_dec_float_50(k))*(exp(x) + exp(t)));
+    return pow(t, k) / (boost::math::tgamma(bmp_real(k))*(exp(x) + exp(t)));
 }
 
 // хочется сделать одну функцию, которая будет подтягивать коэффиуциенты в зависимости от индекса
-static cpp_dec_float_50 get_negative_value(const cpp_dec_float_50 x, const cpp_dec_float_50 k)
+static bmp_real get_negative_value(const bmp_real x, const bmp_real k)
 {
     using namespace coefficients;
     const size_t N_base = 4;
-    cpp_dec_float_50 S1 = 0, S2 = 0;
-    cpp_dec_float_50 I_negative;
-    cpp_dec_float_50 y = log(1 + exp(x));
+    bmp_real S1 = 0, S2 = 0;
+    bmp_real I_negative;
+    bmp_real y = log(1 + exp(x));
 
     for (int n = 0; n < N_base + 1; n++) {
         S1 = S1 + a_k1[n]*pow(y, n + 1);
@@ -84,47 +86,46 @@ static cpp_dec_float_50 get_negative_value(const cpp_dec_float_50 x, const cpp_d
     }
 
     I_negative = (1 + S1) / (1 + S2);
-    //delta_base.at(j) = (F_base.at(j) / z.at(j) - 1);
     return I_negative;
 }
 
-cpp_dec_float_50 fdsf::integer::fd_one(cpp_dec_float_50 x)
+bmp_real fdsf::integer::fd_one(bmp_real x)
 {
-    const cpp_dec_float_50 I_1_0 = PI*PI / 12;
+    const bmp_real I_1_0 = PI*PI / 12;
     const int k = 1;
   
-    cpp_dec_float_50 I_1_minus_x = get_negative_value(-x, k);
+    bmp_real I_1_minus_x = get_negative_value(-x, k);
 
     return x*x / 2 + 2*I_1_0 - I_1_minus_x;
 }
 
-cpp_dec_float_50 fdsf::integer::fd_two(cpp_dec_float_50 x)
+bmp_real fdsf::integer::fd_two(bmp_real x)
 {
-    const cpp_dec_float_50 I_1_0 = PI*PI / 12.0;
+    const bmp_real I_1_0 = PI*PI / 12.0;
     const int k = 2;
 
-    cpp_dec_float_50 I_2_minus_x = get_negative_value(-x, k);
+    bmp_real I_2_minus_x = get_negative_value(-x, k);
 
     return x*x*x / 3 + 4*x*I_1_0 + I_2_minus_x;
 }
 
-cpp_dec_float_50 fdsf::integer::fd_3(cpp_dec_float_50 x)
+bmp_real fdsf::integer::fd_3(bmp_real x)
 {
-    const cpp_dec_float_50 I_1_0 = PI*PI / 12.0;
-    const cpp_dec_float_50 I_3_0 = 7 * PI*PI*PI*PI / 120;
+    const bmp_real I_1_0 = PI*PI / 12.0;
+    const bmp_real I_3_0 = 7 * PI*PI*PI*PI / 120;
     const int k = 3;
 
-    cpp_dec_float_50 I_3_minus_x = get_negative_value(-x, k);
+    bmp_real I_3_minus_x = get_negative_value(-x, k);
 
     return x*x*x*x / 4 + 6*x*x*I_1_0 + 2*I_3_0 - I_3_minus_x;
 }
 
-cpp_dec_float_50 fdsf::integer::fd_4(const cpp_dec_float_50 x)
+bmp_real fdsf::integer::fd_4(const bmp_real x)
 {
-    const cpp_dec_float_50 I_1_0 = PI*PI / 12.0;
-    const cpp_dec_float_50 I_3_0 = 7 * PI*PI*PI*PI / 120;
+    const bmp_real I_1_0 = PI*PI / 12.0;
+    const bmp_real I_3_0 = 7 * PI*PI*PI*PI / 120;
     const int k = 4;
 
-    cpp_dec_float_50 I_4_minus_x = get_negative_value(-x, k);
+    bmp_real I_4_minus_x = get_negative_value(-x, k);
     return x*x*x*x*x/5 + 8*x*x*x*I_1_0 + 8*x*I_3_0 + I_4_minus_x;
 }
--- fdsf.h
+++ fdsf.h
@@ -8,22 +8,20 @@
 namespace fdsf{
     using namespace boost::multiprecision;
 
-    // Вычисляет значение функции ФД индекса k = 1, 2, 3 в точке x при заданном
-    // значении t. Функция представлена в виде t^k /(exp(x)+exp(t))
-    cpp_dec_float_50 FermiDirakFunction(cpp_dec_float_50 t, cpp_dec_float_50 x, cpp_dec_float_50 k);
+    typedef cpp_dec_float_50 bmp_real;
 
     namespace integer {
         // Вычисляет значение функции ФД индекса k=1 в точке x
-        cpp_dec_float_50 fd_one(cpp_dec_float_50 x);
+        bmp_real fd_one(bmp_real x);
 
         // Вычисляет значение функции ФД индекса k=2 в точке x
-        cpp_dec_float_50 fd_two(cpp_dec_float_50 x);
+        bmp_real fd_two(bmp_real x);
 
         // Вычисляет значение функции ФД индекса k=3 в точке x
-        cpp_dec_float_50 fd_3(cpp_dec_float_50 x);
+        bmp_real fd_3(bmp_real x);
 
         // Вычисляет значение функции ФД индекса k=4 в точке x
-        cpp_dec_float_50 fd_4(cpp_dec_float_50 x);
+        bmp_real fd_4(bmp_real x);
     } // namespace integer
 
 } // namespace fdsf
