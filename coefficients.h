7b4a586b053fa77944cc3a19e5c9e771457fcbe3
semenak94 <semenak94@mail.ru>
2016-06-28 13:35:37 +0300

    Src converted to UTF-8

--- coefficients.h
+++ coefficients.h
@@ -1,12 +1,17 @@
-#pragma once
+﻿#pragma once
 
 #include <vector>
 #include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/math/special_functions/gamma.hpp>
 
-namespace coefficients {
+namespace constants {
     using namespace boost::multiprecision;
 
+    typedef cpp_dec_float_50 bmp_real;
     typedef std::vector<cpp_dec_float_50> mp_coefficients;
+
+    // Значение pi
+    const bmp_real mp_pi = boost::math::constants::pi<bmp_real>();
 
     const mp_coefficients a_k1 = { 0.2715113138214362780964488,
                                    0.0562661238060587633169245,
--- fdsf.cpp
+++ fdsf.cpp
@@ -1,131 +1,137 @@
-#include "fdsf.h"
+п»ї#include "fdsf.h"
 #include "coefficients.h"
-#include <boost/math/special_functions/gamma.hpp>
 #include <iomanip>
 #include <limits>
 
-using namespace fdsf;
+namespace fdsf {
 
-// Значение pi
-const bmp_real PI = boost::math::constants::pi<bmp_real>();
-
-// Значение Ik(0) для индекса k = 0,1,2,3
-//const static bmp_real I_k_0[] = { log(bmp_real(2))
+    // Р—РЅР°С‡РµРЅРёРµ Ik(0) РґР»СЏ РёРЅРґРµРєСЃР° k = 0,1,2,3
+    //const static bmp_real I_k_0[] = { log(bmp_real(2))
 #if 0
-bmp_real fdsf::get_T_max(bmp_real X, int k)
-{
-    bmp_real a1;
-    if (k != 0) {
-        a1 = pow(fdsf::factorial(k + 1), -1 / k);
-    }
-    int i = 1;
-    bmp_real y = log(1 + exp(X));
-    bmp_real T = X - log(epsilon), I_approximate;
-    while (true)
+    bmp_real get_T_max(bmp_real X, int k)
     {
-        // 3-х итераций вполне достаточно для определения Tmax
-        if (i > 3) {
-            break;
+        bmp_real a1;
+        if (k != 0) {
+            a1 = pow(fdsf::factorial(k + 1), -1 / k);
         }
-        if (k == 0) {
-            I_approximate = y;
+        int i = 1;
+        bmp_real y = log(1 + exp(X));
+        bmp_real T = X - log(epsilon), I_approximate;
+        while (true)
+        {
+            // 3-С… РёС‚РµСЂР°С†РёР№ РІРїРѕР»РЅРµ РґРѕСЃС‚Р°С‚РѕС‡РЅРѕ РґР»СЏ РѕРїСЂРµРґРµР»РµРЅРёСЏ Tmax
+            if (i > 3) {
+                break;
+            }
+            if (k == 0) {
+                I_approximate = y;
+            }
+            else {
+                I_approximate = fdsf::factorial(k)*y*pow(1 + a1*y, k);
+            }
+            // РС‚РµСЂР°С†РёРѕРЅРЅРѕРµ РІС‹С‡РёСЃР»РµРЅРёРµ Tmax
+            T = X - log(epsilon) + log(pow(T + 1, k) / I_approximate);
+            i++;
         }
-        else {
-            I_approximate = fdsf::factorial(k)*y*pow(1 + a1*y, k);
-        }
-        // Итерационное вычисление Tmax
-        T = X - log(epsilon) + log(pow(T + 1, k) / I_approximate);
-        i++;
-    }
 
-    return sqrt(T);
-}
+        return sqrt(T);
+    }
 #endif
 
-// Новая схема Горнера для прецизионного вычисления y
-static bmp_real GornerSchemeForPrecesionY(int N, bmp_real x)
-{
-    //bmp_real alpha = x < epsilon ? exp(x) : exp(-x);
-    const bmp_real alpha = exp(x);
-    const bmp_real z = alpha / (2 + alpha);
-    bmp_real sum = 1.0 / (2 * N + 1);
+    // РќРѕРІР°СЏ СЃС…РµРјР° Р“РѕСЂРЅРµСЂР° РґР»СЏ РїСЂРµС†РёР·РёРѕРЅРЅРѕРіРѕ РІС‹С‡РёСЃР»РµРЅРёСЏ y
+    static bmp_real GornerSchemeForPrecesionY(int N, bmp_real x)
+    {
+        //bmp_real alpha = x < epsilon ? exp(x) : exp(-x);
+        const bmp_real alpha = exp(x);
+        const bmp_real z = alpha / (2 + alpha);
+        bmp_real sum = 1.0 / (2 * N + 1);
 
-    for (int i = N - 1; i >= 0; i--) {
-        sum = 1.0 / (2 * i + 1.0) + z*z*sum;
+        for (int i = N - 1; i >= 0; i--) {
+            sum = 1.0 / (2 * i + 1.0) + z*z*sum;
+        }
+
+        //return x < epsilon ? 2*z*sum : x + 2*z*sum;
+        return 2 * z*sum;
     }
 
-    //return x < epsilon ? 2*z*sum : x + 2*z*sum;
-    return 2 * z*sum;
-}
-
-// Вычисляет значение функции ФД индекса k = 1, 2, 3 в точке x при заданном
-// значении t. Функция представлена в виде t^k /(exp(x)+exp(t))
-static bmp_real FermiDirakFunction(bmp_real t, bmp_real x, bmp_real k)
-{
+    // Р’С‹С‡РёСЃР»СЏРµС‚ Р·РЅР°С‡РµРЅРёРµ С„СѓРЅРєС†РёРё Р¤Р” РёРЅРґРµРєСЃР° k = 1, 2, 3 РІ С‚РѕС‡РєРµ x РїСЂРё Р·Р°РґР°РЅРЅРѕРј
+    // Р·РЅР°С‡РµРЅРёРё t. Р¤СѓРЅРєС†РёСЏ РїСЂРµРґСЃС‚Р°РІР»РµРЅР° РІ РІРёРґРµ t^k /(exp(x)+exp(t))
+    static bmp_real FermiDirakFunction(bmp_real t, bmp_real x, bmp_real k)
+    {
 #if 0
-    // new , зачин для полуцелых
-    return 2 * pow(t, 2 * k + 1) / (1 + exp(t*t - x));
+        // new , Р·Р°С‡РёРЅ РґР»СЏ РїРѕР»СѓС†РµР»С‹С…
+        return 2 * pow(t, 2 * k + 1) / (1 + exp(t*t - x));
 #endif
-    return pow(t, k) / (boost::math::tgamma(bmp_real(k))*(exp(x) + exp(t)));
-}
-
-// хочется сделать одну функцию, которая будет подтягивать коэффиуциенты в зависимости от индекса
-static bmp_real get_negative_value(const bmp_real x, const bmp_real k)
-{
-    using namespace coefficients;
-    const size_t N_base = 4;
-    bmp_real S1 = 0, S2 = 0;
-    bmp_real I_negative;
-    bmp_real y = log(1 + exp(x));
-
-    for (int n = 0; n < N_base + 1; n++) {
-        S1 = S1 + a_k1[n]*pow(y, n + 1);
-    }
-    for (int m = 0; m < N_base; m++) {
-        S2 = S2 + b_k1[m]*pow(y, m + 1);
+        return pow(t, k) / (boost::math::tgamma(bmp_real(k))*(exp(x) + exp(t)));
     }
 
-    I_negative = (1 + S1) / (1 + S2);
-    return I_negative;
-}
+    // С…РѕС‡РµС‚СЃСЏ СЃРґРµР»Р°С‚СЊ РѕРґРЅСѓ С„СѓРЅРєС†РёСЋ, РєРѕС‚РѕСЂР°СЏ Р±СѓРґРµС‚ РїРѕРґС‚СЏРіРёРІР°С‚СЊ РєРѕСЌС„С„РёСѓС†РёРµРЅС‚С‹ РІ Р·Р°РІРёСЃРёРјРѕСЃС‚Рё РѕС‚ РёРЅРґРµРєСЃР°
+    static bmp_real get_negative_value(const bmp_real x, const bmp_real k,
+        const constants::mp_coefficients& a,
+        const constants::mp_coefficients& b)
+    {
 
-bmp_real fdsf::integer::fd_one(bmp_real x)
-{
-    const bmp_real I_1_0 = PI*PI / 12;
-    const int k = 1;
-  
-    bmp_real I_1_minus_x = get_negative_value(-x, k);
+        const size_t N_base = 4;
+        bmp_real S1 = 0, S2 = 0;
+        bmp_real I_negative;
+        bmp_real y = log(1 + exp(x));
 
-    return x*x / 2 + 2*I_1_0 - I_1_minus_x;
-}
+        using namespace constants;
 
-bmp_real fdsf::integer::fd_two(bmp_real x)
-{
-    const bmp_real I_1_0 = PI*PI / 12.0;
-    const int k = 2;
+        for (int n = 0; n < N_base + 1; n++) {
+            S1 = S1 + a[n] * pow(y, n + 1);
+        }
+        for (int m = 0; m < N_base; m++) {
+            S2 = S2 + b[m] * pow(y, m + 1);
+        }
 
-    bmp_real I_2_minus_x = get_negative_value(-x, k);
+        I_negative = (1 + S1) / (1 + S2);
+        // РІРѕР·РІСЂР°С‰Р°РµРј Р·РЅР°С‡РµРЅРёРµ Р°РїРїСЂРѕРєСЃРёРјР°С†РёРё СЃРѕРіР»Р°СЃРЅРѕ С„РѕСЂРјСѓР»Рµ (14) РІ СЃС‚Р°С‚СЊРµ
+        return boost::math::tgamma(k + 1)*y*pow(I_negative, k);
+    }
 
-    return x*x*x / 3 + 4*x*I_1_0 + I_2_minus_x;
-}
+    bmp_real integer::fd_one(const bmp_real x)
+    {
+        using namespace constants;
+        const bmp_real I_1_0 = mp_pi*mp_pi / 12;
+        const int k = 1;
 
-bmp_real fdsf::integer::fd_3(bmp_real x)
-{
-    const bmp_real I_1_0 = PI*PI / 12.0;
-    const bmp_real I_3_0 = 7 * PI*PI*PI*PI / 120;
-    const int k = 3;
+        bmp_real I_1_minus_x = get_negative_value(-x, k, constants::a_k1, constants::b_k1);
 
-    bmp_real I_3_minus_x = get_negative_value(-x, k);
+        return x*x / 2 + 2 * I_1_0 - I_1_minus_x;
+    }
 
-    return x*x*x*x / 4 + 6*x*x*I_1_0 + 2*I_3_0 - I_3_minus_x;
-}
+    bmp_real integer::fd_two(const bmp_real x)
+    {
+        using namespace constants;
+        const bmp_real I_1_0 = mp_pi*mp_pi / 12.0;
+        const int k = 2;
 
-bmp_real fdsf::integer::fd_4(const bmp_real x)
-{
-    const bmp_real I_1_0 = PI*PI / 12.0;
-    const bmp_real I_3_0 = 7 * PI*PI*PI*PI / 120;
-    const int k = 4;
+        bmp_real I_2_minus_x = get_negative_value(-x, k, a_k2, b_k2);
 
-    bmp_real I_4_minus_x = get_negative_value(-x, k);
-    return x*x*x*x*x/5 + 8*x*x*x*I_1_0 + 8*x*I_3_0 + I_4_minus_x;
+        return x*x*x / 3 + 4 * x*I_1_0 + I_2_minus_x;
+    }
+
+    bmp_real integer::fd_3(const bmp_real x)
+    {
+        using namespace constants;
+        const bmp_real I_1_0 = mp_pi*mp_pi / 12.0;
+        const bmp_real I_3_0 = 7 * mp_pi*mp_pi*mp_pi*mp_pi / 120;
+        const int k = 3;
+
+        bmp_real I_3_minus_x = get_negative_value(-x, k, a_k3, b_k3);
+
+        return x*x*x*x / 4 + 6 * x*x*I_1_0 + 2 * I_3_0 - I_3_minus_x;
+    }
+
+    bmp_real integer::fd_4(const bmp_real x)
+    {
+        using namespace constants;
+        const bmp_real I_1_0 = mp_pi*mp_pi / 12.0;
+        const bmp_real I_3_0 = 7 * mp_pi*mp_pi*mp_pi*mp_pi / 120;
+        const int k = 4;
+
+        bmp_real I_4_minus_x = get_negative_value(-x, k, a_k4, b_k4);
+        return x*x*x*x*x / 5 + 8 * x*x*x*I_1_0 + 8 * x*I_3_0 + I_4_minus_x;
+    }
 }
